174964db784b9abe9d5355c56cd004738b01466e
Safwan MI <saffanmd.cs19@bmsce.ac.in>
2021-07-25 04:02:32 +0530

    added name to answers

--- src/components/Answers/apost/apost.js
+++ src/components/Answers/apost/apost.js
@@ -6,27 +6,33 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
 import {useState,useEffect} from 'react';
 export default function APost({user_id,answer,question_id}) {
-    // const [Name,setName] = useState([]);
-    // useEffect(()=>{//Will be called as soon as the page renders.
-    //     /*********/   
-    //             const getName=async()=>{
-    //                 const res=await fetch('http://localhost:3001/getUser/'+user_id)
-    //                 setName(res[0])  
-    //                 console.log(res.name)
-    //             }
-    //             getName();
-    //     /********/
+    const [Name,setName] = useState('');
+    useEffect(()=>{//Will be called as soon as the page renders.
+        /*********/   
+                const getName=async()=>{
+                    
+                    const res=await fetch('https://pe-exp-api.herokuapp.com/getUser/'+user_id)
+                    const data=await res.json()
+                
+                    setName(data.name)  
+                }
+                getName();
+        /********/
         
-    //           },[])
+              },[])
     return (
         <div className="apost">
             <div className="apostWrapper">
             
             <div className="apostTop">
+            <a href='/profile' onClick={()=>{
+                localStorage.setItem('profile_id',user_id)
+            }}>
                 <div className="apostTopLeft">
                 <AccountCircleIcon style={{fontSize:50}}className="apostProfileImage"/>
-                {/* <span className="apostUsername">{Name}</span> */}
+                <span className="apostUsername">{Name}</span>
                 </div>
+            </a>
             </div>
 
             <div className="apostCenter">
